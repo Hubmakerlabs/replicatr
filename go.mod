72e0b0c9c888d34191870f3b966c2f7329b721a1
mleku <mlekugfy@gmail.com>
2023-12-01 10:15:18 +0000

    add websocket conn

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module mleku.online/git/replicatr
 go 1.21.4
 
 require (
-	github.com/mailru/easyjson v0.7.7
+	github.com/gobwas/httphead v0.1.0
+	github.com/gobwas/ws v1.3.1
 	github.com/minio/sha256-simd v1.0.1
 	mleku.online/git/ec v1.0.3
 	mleku.online/git/log v1.0.4
@@ -12,8 +13,8 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dchest/blake256 v1.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gookit/color v1.5.4 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.3 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/sys v0.10.0 // indirect
---
+++ pkg/nostr/websocket/connection.go
@@ -0,0 +1,188 @@
+package nostr
+
+import (
+	"bytes"
+	"compress/flate"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+
+	"github.com/gobwas/httphead"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsflate"
+	"github.com/gobwas/ws/wsutil"
+)
+
+// WebsocketConn is a nostr websocket connection.
+type WebsocketConn struct {
+	conn              net.Conn
+	enableCompression bool
+	controlHandler    wsutil.FrameHandlerFunc
+	flateReader       *wsflate.Reader
+	reader            *wsutil.Reader
+	flateWriter       *wsflate.Writer
+	writer            *wsutil.Writer
+	msgStateR         wsflate.MessageState
+	msgStateW         wsflate.MessageState
+	error
+}
+
+// NewWebsocket makes a new WebsocketConn connected to the provided url using
+// teh provided http.Header.
+func NewWebsocket(ctx context.Context, url string,
+	reqHdr http.Header) (c *WebsocketConn, e error) {
+
+	c = &WebsocketConn{}
+
+	dialer := ws.Dialer{
+		Header: ws.HandshakeHeaderHTTP(reqHdr),
+		Extensions: []httphead.Option{
+			wsflate.DefaultParameters.Option(),
+		},
+	}
+
+	var hs ws.Handshake
+	if c.conn, _, hs, e = dialer.Dial(ctx, url); e != nil {
+		return nil, fmt.Errorf("failed to dial: %w", e)
+	}
+
+	state := ws.StateClientSide
+	for _, extension := range hs.Extensions {
+
+		if string(extension.Name) == wsflate.ExtensionName {
+			c.enableCompression = true
+			state |= ws.StateExtended
+			break
+		}
+	}
+
+	if c.enableCompression {
+
+		c.msgStateR.SetCompressed(true)
+
+		c.flateReader = wsflate.NewReader(nil,
+			func(r io.Reader) wsflate.Decompressor {
+				return flate.NewReader(r)
+			},
+		)
+	}
+
+	c.controlHandler = wsutil.ControlFrameHandler(c.conn, ws.StateClientSide)
+
+	c.reader = &wsutil.Reader{
+		Source:         c.conn,
+		State:          state,
+		OnIntermediate: c.controlHandler,
+		CheckUTF8:      false,
+		Extensions: []wsutil.RecvExtension{
+			&c.msgStateR,
+		},
+	}
+
+	if c.enableCompression {
+
+		c.msgStateW.SetCompressed(true)
+
+		c.flateWriter = wsflate.NewWriter(nil,
+			func(w io.Writer) (fw wsflate.Compressor) {
+				fw, c.error = flate.NewWriter(w, 4)
+				if c.error != nil {
+					// this error doesn't go anywhere and there is no logger
+					// here. error will be stored in the connection.
+					c.error = fmt.Errorf("failed to create flate writer: %v", e)
+				}
+				return fw
+			},
+		)
+	}
+	c.writer = wsutil.NewWriter(c.conn, state, ws.OpText)
+	c.writer.SetExtensions(&c.msgStateW)
+	return
+}
+
+// WriteMessage dispatches bytes to the websocket WebsocketConn.
+func (c *WebsocketConn) WriteMessage(data []byte) (e error) {
+
+	dataReader := bytes.NewReader(data)
+
+	if c.msgStateW.IsCompressed() && c.enableCompression {
+
+		c.flateWriter.Reset(c.writer)
+
+		if _, e = io.Copy(c.flateWriter, dataReader); e != nil {
+			return fmt.Errorf("failed to write message: %w", e)
+
+		} else if e = c.flateWriter.Close(); e != nil {
+			return fmt.Errorf("failed to close flate writer: %w", e)
+		}
+
+	} else if _, e = io.Copy(c.writer, dataReader); e != nil {
+		return fmt.Errorf("failed to write message: %w", e)
+
+	} else if e = c.writer.Flush(); e != nil {
+		return fmt.Errorf("failed to flush writer: %w", e)
+	}
+	return
+}
+
+// ReadMessage returns the next message that arrives on the WebsocketConn.
+func (c *WebsocketConn) ReadMessage(ctx context.Context,
+	buf io.Writer) (e error) {
+
+	for {
+		select {
+		case <-ctx.Done():
+			return errors.New("context canceled")
+		default:
+		}
+
+		var h ws.Header
+		if h, e = c.reader.NextFrame(); e != nil {
+
+			// if there is an error closing c.Check() returns true and c.Error()
+			// returns the error text. (previously this used a logger).
+			c.error = c.conn.Close()
+
+			return fmt.Errorf("failed to advance frame: %w", e)
+		}
+
+		if h.OpCode.IsControl() {
+
+			if err := c.controlHandler(h, c.reader); err != nil {
+				return fmt.Errorf("failed to handle control frame: %w", err)
+			}
+
+		} else if h.OpCode == ws.OpBinary || h.OpCode == ws.OpText {
+			break
+		}
+
+		if e = c.reader.Discard(); e != nil {
+			return fmt.Errorf("failed to discard: %w", e)
+		}
+	}
+
+	if c.msgStateR.IsCompressed() && c.enableCompression {
+
+		c.flateReader.Reset(c.reader)
+
+		if _, err := io.Copy(buf, c.flateReader); err != nil {
+			return fmt.Errorf("failed to read message: %w", err)
+		}
+	} else {
+		if _, err := io.Copy(buf, c.reader); err != nil {
+			return fmt.Errorf("failed to read message: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (c *WebsocketConn) Close() error { return c.conn.Close() }
+
+// Check returns the internal error state. If it returns true, the text of the
+// error can be accessed with the Error() method because the `error` is
+// embedded.
+func (c *WebsocketConn) Check() bool { return c.error != nil }
