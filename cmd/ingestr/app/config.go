9caacf239a3c06dd396b4e9536589f927028b1dc
mleku <mlekugfy@gmail.com>
2024-02-05 08:29:50 +0000

    tidied up parameters
    Former-commit-id: daa608c2ecb7dac85620e209b3c064ddd678dc6d [formerly 613d1e7cbdebe29c18af1ef4e5262e10ca150dd9]
    Former-commit-id: f8ba69d35129a21eb40ae4a25e040f197b2a9aee

--- cmd/ingestr/app/config.go
+++ cmd/ingestr/app/config.go
@@ -14,13 +14,13 @@ var log = slog.New(os.Stderr, "app/"+Name)
 
 type Config struct {
 	Nsec          string  `arg:"-n,--nsec" json:"nsec" help:"use the nsec (bech32 encoded) for auth and if given, writes it to configuration and will be loaded afterwards until a new one is given"`
-	UploadRelay   string  `arg:"positional,required" json:"-"`
 	DownloadRelay string  `arg:"positional,required" json:"-"`
+	UploadRelay   string  `arg:"positional,required" json:"-"`
 	Since         int64   `arg:"-s,--since" json:"-" help:"only query events since this unix timestamp"`
 	PubkeyHex     string  `arg:"-" json:"-"`
 	SeckeyHex     string  `arg:"-" json:"-"`
 	Kinds         kinds.T `arg:"-k,--kinds" help:"comma separated list of kind numbers to ingest"`
-	Limit         int     `arg:"-l,--limit" help:"maximum of number of events to return" default:"100"`
+	Limit         int     `arg:"-l,--limit" help:"maximum of number of events to return for each hour long interval" default:"1000"`
 }
 
 var defaultKinds = kinds.T{kind.TextNote}
--- cmd/ingestr/app/ingest.go
+++ cmd/ingestr/app/ingest.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Hubmakerlabs/replicatr/pkg/nostr/relay"
 	"github.com/Hubmakerlabs/replicatr/pkg/nostr/subscription"
 	"github.com/Hubmakerlabs/replicatr/pkg/nostr/tag"
+	"github.com/Hubmakerlabs/replicatr/pkg/nostr/timestamp"
 )
 
 func Ingest(args *Config) int {
@@ -23,88 +24,114 @@ func Ingest(args *Config) int {
 	if len(args.Kinds) == 0 {
 		args.Kinds = defaultKinds
 	}
-	log.I.F("ingesting events of kind %v with dates after %v",
-		args.Kinds, time.Unix(args.Since, 0))
+	log.I.F("ingesting events of kind %v with dates after %v from %s and sending to %s",
+		args.Kinds,
+		time.Unix(args.Since, 0),
+		args.DownloadRelay,
+		args.UploadRelay,
+	)
 	c := context.Bg()
 	var err error
-	var downRelay *relay.T
+	var downRelay, upRelay *relay.T
 	if downRelay, err = relay.Connect(c, args.DownloadRelay); log.E.Chk(err) {
 		return 1
 	}
-	// create the subscription to the download relay
-	var sub *subscription.T
-	f := filters.T{
-		{
-			Kinds:   args.Kinds,
-			Authors: tag.T{args.PubkeyHex},
-			Limit:   args.Limit,
-		},
-		{
-			Kinds: args.Kinds,
-			Tags:  filter.TagMap{"#p": {args.PubkeyHex}},
-			Limit: args.Limit,
-		},
-	}
-	if sub, err = downRelay.Subscribe(c, f); log.Fail(err) {
-		// this should fail
-	}
-	select {
-	case <-downRelay.AuthRequired:
-		if err = downRelay.Auth(c, func(evt *event.T) error { return evt.Sign(args.SeckeyHex) }); log.Fail(err) {
-			return 1
-		}
-	case <-time.After(2 * time.Second):
-	}
-	if sub, err = downRelay.Subscribe(c, f,
-		subscription.WithLabel(fmt.Sprint(time.Now().Unix()))); log.Fail(err) {
+	if upRelay, err = relay.Connect(c, args.UploadRelay); log.E.Chk(err) {
 		return 1
 	}
-	if sub == nil {
-		log.E.Ln("subscription failed to start")
-		return 1
-	}
-	var upRelay *relay.T
-	if upRelay, err = relay.Connect(c, args.DownloadRelay); log.E.Chk(err) {
-		return 1
-	}
-	uc := context.Bg()
+	_, _ = downRelay, upRelay
 	var count int
-out:
-	for {
-		log.D.Ln("receiving event")
-		select {
-		case <-c.Done():
-			break out
-		case <-sub.EndOfStoredEvents:
-			sub.Unsub()
-			break out
-		case ev, more := <-sub.Events:
-			if !more {
-				break out
-			}
-			if ev == nil {
-				break out
-			}
-			count++
-			if err = upRelay.Publish(uc, ev); log.Fail(err) {
-				// this can fail once
-				select {
-				case <-upRelay.AuthRequired:
-					if err = downRelay.Auth(c,
-						func(evt *event.T) error {
-							return evt.Sign(args.SeckeyHex)
-						}); log.Fail(err) {
-						return 1
-					}
-					if err = upRelay.Publish(uc, ev); log.Fail(err) {
-						return 1
-					}
-				case <-time.After(2 * time.Second):
-					log.E.Ln("timed out waiting to auth")
+	oldest := args.Since
+	now := time.Now().Unix()
+	var downAuthed, upAuthed bool
+	var increment int64 = 60 * 60
+	for i := oldest; i < now; i += increment {
+		// create the subscription to the download relay
+		var sub *subscription.T
+		since := timestamp.FromUnix(i).Ptr()
+		until := timestamp.FromUnix(i + increment - 1).Ptr()
+		f := filters.T{
+			{
+				Kinds:   args.Kinds,
+				Authors: tag.T{args.PubkeyHex},
+				Limit:   args.Limit,
+				Since:   since,
+				Until:   until,
+			},
+			{
+				Kinds: args.Kinds,
+				Tags:  filter.TagMap{"#p": {args.PubkeyHex}},
+				Limit: args.Limit,
+				Since: since,
+				Until: until,
+			},
+		}
+		if sub, err = downRelay.Subscribe(c, f); log.Fail(err) {
+			// this could fail
+		}
+		if !downAuthed {
+			select {
+			case <-downRelay.AuthRequired:
+				if err = downRelay.Auth(c, func(evt *event.T) error { return evt.Sign(args.SeckeyHex) }); log.Fail(err) {
 					return 1
 				}
-				log.I.Ln("authed")
-				return 0
+				downAuthed = true
+			case <-time.After(2 * time.Second):
+			}
+			if sub, err = downRelay.Subscribe(c, f,
+				subscription.WithLabel(fmt.Sprint(time.Now().Unix()))); log.Fail(err) {
+				return 1
+			}
+		}
+		if sub == nil {
+			log.E.Ln("subscription failed to start")
+			return 1
+		}
+		uc := context.Bg()
+	out:
+		for {
+			log.D.Ln("receiving event")
+			select {
+			case <-c.Done():
+				break out
+			case <-sub.EndOfStoredEvents:
+				sub.Unsub()
+				break out
+			case ev, more := <-sub.Events:
+				if !more {
+					break out
+				}
+				if ev == nil {
+					break out
+				}
+				count++
+				if err = upRelay.Publish(uc, ev); log.Fail(err) {
+					if !upAuthed {
+						// this can fail once
+						select {
+						case <-upRelay.AuthRequired:
+							if err = upRelay.Auth(c,
+								func(evt *event.T) error {
+									return evt.Sign(args.SeckeyHex)
+								}); log.Fail(err) {
+								return 1
+							}
+							upAuthed = true
+							if err = upRelay.Publish(uc, ev); log.Fail(err) {
+								return 1
+							}
+						case <-time.After(2 * time.Second):
+							log.E.Ln("timed out waiting to auth")
+							return 1
+						}
+						log.I.Ln("authed")
+						return 0
+					} else {
+						if err = upRelay.Publish(uc, ev); log.Fail(err) {
+							return 1
+						}
+					}
+				}
 			}
 		}
 	}
