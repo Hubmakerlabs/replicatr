f8050cab700330906fe762e03ecf8543a14ab9dc
mleku <mlekugfy@gmail.com>
2024-02-06 10:36:06 +0000

    fixed wrong signatures
    Former-commit-id: 94f48569b15d9b18fdfc7c55c7c10be4dbea698a [formerly 820ae56f9e52dff7130e0c59240e449071fcffd1]
    Former-commit-id: 0502de704acddb8bc03c15fe097c48278c77a0b7

--- cmd/nak/example_test.go
+++ cmd/nak/example_test.go
@@ -3,13 +3,13 @@ package main
 func ExampleEventBasic() {
 	app.Run([]string{"nak", "event", "--ts", "1699485669"})
 	// Output:
-	// {"id":"8c848702541668af204bcbbb4d7a760fef0d936d946c1260eaba0d8074ca12ac","pubkey":"79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798","created_at":1699485669,"kind":1,"tags":[],"content":"hello from the nostr army knife","sig":"26e20e69605f08ca7d3e6768ff40232eda31fb550dd7fd4158fa33ad060b1a062c2b6409059374f1ad32867ac0d37581d9cae70ceac08cbc599d9462c9b254ec"}
+	// {"id":"36d88cf5fcc449f2390a424907023eda7a74278120eebab8d02797cd92e7e29c","pubkey":"79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798","created_at":1699485669,"kind":1,"tags":[],"content":"hello from the nostr army knife","sig":"68e71a192e8abcf8582a222434ac823ecc50607450ebe8cc4c145eb047794cc382dc3f888ce879d2f404f5ba6085a47601360a0fa2dd4b50d317bd0c6197c2c2"}
 }
 
 func ExampleEventComplex() {
 	app.Run([]string{"nak", "event", "--ts", "1699485669", "-k", "11", "-c", "skjdbaskd", "--sec", "17", "-t", "t=spam", "-e", "36d88cf5fcc449f2390a424907023eda7a74278120eebab8d02797cd92e7e29c", "-t", "r=https://abc.def?name=foobar;nothing"})
 	// Output:
-	// {"id":"33159e38e02a903478b7189adc528d4f478c7231ce8c73031f7ee4787327227b","pubkey":"2fa2104d6b38d11b0230010559879124e42ab8dfeff5ff29dc9cdadd4ecacc3f","created_at":1699485669,"kind":11,"tags":[["t","spam"],["r","https://abc.def?name=foobar","nothing"],["e","36d88cf5fcc449f2390a424907023eda7a74278120eebab8d02797cd92e7e29c"]],"content":"skjdbaskd","sig":"be36888db7b3effc79a27d04837fb25be008d5a2c0b1c0642102dda1631c499d9a9ba176d713554c156723d8de5e72eee51099990ebcf62ff0f84b4b31462699"}
+	// {"id":"19aba166dcf354bf5ef64f4afe69ada1eb851495001ee05e07d393ee8c8ea179","pubkey":"2fa2104d6b38d11b0230010559879124e42ab8dfeff5ff29dc9cdadd4ecacc3f","created_at":1699485669,"kind":11,"tags":[["t","spam"],["r","https://abc.def?name=foobar","nothing"],["e","36d88cf5fcc449f2390a424907023eda7a74278120eebab8d02797cd92e7e29c"]],"content":"skjdbaskd","sig":"cf452def4a68341c897c3fc96fa34dc6895a5b8cc266d4c041bcdf758ec992ec5adb8b0179e98552aaaf9450526a26d7e62e413b15b1c57e0cfc8db6b29215d7"}
 }
 
 func ExampleReq() {
