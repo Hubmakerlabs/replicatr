e523da0b97b23f08dbb977c2c1300070b9e0a048
akashkumar1691 <akashkumar1691@berkeley.edu>
2024-02-20 14:14:39 -0800

    tookout testnet

--- ic/testnet/setup/local_testnet_setup_pt1.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install cmake
-sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-nvm install 18
-dfx start
