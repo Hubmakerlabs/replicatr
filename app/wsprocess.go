5fa511fe280a3651554b23e49cc6880f7e1dd5e3
mleku <mlekugfy@gmail.com>
2024-02-21 11:09:54 +0000

    fixed error in hyperlink goland invocation
    the --line option has to be BEFORE otherwise no idea what basis it makes for which line to load at
    
    
    Former-commit-id: 6eb2491fcbea2e3d708cd512f60af53395d56503 [formerly 61a88507d3671c14ee04feceee37dd1a87e8cd76]
    Former-commit-id: 69594a1df7390498d75c5b4f991a73030e0c46ad

--- app/wsprocess.go
+++ app/wsprocess.go
@@ -64,7 +64,7 @@ func (rl *Relay) wsProcessMessages(msg []byte, c context.T,
 	}
 	var en enveloper.I
 	var err error
-	if en, _, err = envelopes.ProcessEnvelope(msg); log.D.Chk(err) {
+	if en, _, err = envelopes.ProcessEnvelope(msg); log.T.Chk(err) {
 		return
 	}
 	if en == nil {
