0f5fccb6c68b1dd816b0889bb387c1cf643f75b0
akashkumar1691 <akashkumar1691@berkeley.edu>
2024-02-21 18:00:18 -0800

    basic create and retrieval functionality when interacting with testnet via terminal

--- pkg/ic/testnet/src/declarations/testnet_backend/testnet_backend.did.d.ts
+++ pkg/ic/testnet/src/declarations/testnet_backend/testnet_backend.did.d.ts
@@ -2,6 +2,10 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface _SERVICE { 'greet' : ActorMethod<[string], string> }
+export interface Record { 'id' : bigint, 'content' : string }
+export interface _SERVICE {
+  'get_record' : ActorMethod<[bigint], [] | [Record]>,
+  'save_record' : ActorMethod<[bigint, string], string>,
+}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
--- pkg/ic/testnet/src/testnet_backend/src/lib.rs
+++ pkg/ic/testnet/src/testnet_backend/src/lib.rs
@@ -1,4 +1,28 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::CandidType;
+use ic_cdk_macros::{update, query};
+use std::cell::RefCell;
+use serde::Deserialize;
+
+#[derive(CandidType, Deserialize, Clone)]
+struct Record {
+    id: u64,
+    content: String,
+}
+
+thread_local! {
+    static RECORDS: RefCell<Vec<Record>> = RefCell::new(Vec::new());
+}
+
+
+#[update]
+fn save_record(id: u64, content: String) -> String{
+    let record = Record { id, content };
+    RECORDS.with(|records| records.borrow_mut().push(record));
+    "sucess".to_string()
+}
+
+
+#[query]
+fn get_record(id: u64) -> Option<Record> {
+    RECORDS.with(|records| records.borrow().iter().find(|r| r.id == id).cloned()) 
 }
