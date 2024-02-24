e2d4beea71a54b34024c58f204fe7ab3252ec0d0
akashkumar1691 <akashkumar1691@berkeley.edu>
2024-02-23 18:33:58 -0800

    implemented save_event and get_events on canister backend
    Former-commit-id: 7f409856dd7659ab9a280fa730ffbf562054cbbc [formerly ba2775a4790a4718f60a4ab9b906d8c2509bf2e5]
    Former-commit-id: 5cdf96f1d6743177c01bed33b320eecbf75270b9

--- pkg/ic/testnet/src/declarations/testnet_backend/testnet_backend.did.d.ts
+++ pkg/ic/testnet/src/declarations/testnet_backend/testnet_backend.did.d.ts
@@ -2,10 +2,29 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface Record { 'id' : bigint, 'content' : string }
+export interface Event {
+  'id' : string,
+  'sig' : string,
+  'content' : string,
+  'kind' : number,
+  'tags' : Array<Array<string>>,
+  'pubkey' : string,
+  'created_at' : bigint,
+}
+export interface Filter {
+  'ids' : Array<string>,
+  'tags' : Array<KeyValuePair>,
+  'search' : string,
+  'limit' : bigint,
+  'since' : bigint,
+  'authors' : Array<string>,
+  'until' : bigint,
+  'kinds' : Uint16Array | number[],
+}
+export interface KeyValuePair { 'key' : string, 'value' : Array<string> }
 export interface _SERVICE {
-  'get_record' : ActorMethod<[bigint], [] | [Record]>,
-  'save_record' : ActorMethod<[bigint, string], string>,
+  'get_events' : ActorMethod<[Filter], Array<Event>>,
+  'save_event' : ActorMethod<[Event], string>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: ({ IDL }: { IDL: IDL }) => IDL.Type[];
