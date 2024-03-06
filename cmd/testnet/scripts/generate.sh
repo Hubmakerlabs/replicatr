c57fc661d08c9ba23a8842f94901eb51c5593f39
Remco <remco@r3m.codes>
2024-03-06 10:09:53 +0100

    remove node modules

---
+++ cmd/testnet/scripts/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+canisters=(
+    "testnet_backend"
+)
+
+echo -e "${GREEN}> $ENV: Generating required files..${NC}"
+dfx generate --network ic
+
+for t in ${canisters[@]}; do
+    echo -e "${GREEN} $ENV > Generating candid for $t..${NC}"
+    cargo test candid -p $t
+done
+
+rm -rf src/declarations
+echo -e "${GREEN} $ENV > Stopping local replica..${NC}"
