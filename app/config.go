51a0c913d3476dcb1d208a0aee90312fc890cade
mleku <me@mleku.dev>
2024-03-27 11:35:00 +0000

    documented all search index keys
    - all timestamps are now 8 bytes
    - all index tag prefixes now have detailed documentation of how they are constructed
    - lots of little todos for later

--- app/config.go
+++ app/config.go
@@ -16,12 +16,14 @@ type ImportCmd struct {
 
 type InitCfg struct{}
 type WipeBDB struct{}
+type RescanAC struct{}
 
 type Config struct {
 	ExportCmd    *ExportCmd `arg:"subcommand:export" json:"-" help:"export database as line structured JSON"`
 	ImportCmd    *ImportCmd `arg:"subcommand:import" json:"-" help:"import data from line structured JSON"`
 	InitCfgCmd   *InitCfg   `arg:"subcommand:initcfg" json:"-" help:"initialize relay configuration files"`
 	Wipe         *WipeBDB   `arg:"subcommand:wipebdb" json:"-" help:"empties database"`
+	Rescan       *RescanAC  `arg:"subcommand:rescan" json:"-" help:"clear and regenerate access counter records"`
 	Listen       string     `arg:"-l,--listen" default:"0.0.0.0:3334" json:"listen" help:"network address to listen on"`
 	EventStore   string     `arg:"-e,--eventstore" default:"ic" json:"eventstore" help:"select event store backend [ic,badger]"`
 	CanisterAddr string     `arg:"-C,--canisteraddr" default:"127.0.0.1:46847" json:"canister_addr" help:"IC canister address to use"`
